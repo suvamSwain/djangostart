d09985e895841b60bd795c418ef828ada934b9d9
Suvam <suvamgpt8@gmail.com>
2024-04-09 19:46:51 +0530

    added files

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path,include
-from .views import main
+from .views import ProjectViewset,main
+from rest_framework.routers import DefaultRouter
 
 urlpatterns = [
-    path('',main)
+    path('/main',main)
 ]
+
+router = DefaultRouter()
+router.register('project',ProjectViewset, basename='project')
+urlpatterns = router.urls
--- api/views.py
+++ api/views.py
@@ -1,6 +1,48 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework import viewsets,permissions
+from .models import *
+from .serializers import *
+from rest_framework.response import Response
 
 # Create your views here.
 def main(Request):
     return HttpResponse("<h1>Here is the code</h1>")
+
+class ProjectViewset(viewsets.ViewSet):
+    #http_method_names = ['post','get','put','delete']
+    permission_classes = [permissions.AllowAny]
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+      
+    def list(self,request):
+        queryset = Project.objects.all()
+        serializer = self.serializer_class(queryset,many=True)
+        return Response(serializer.data)
+    
+    def create(self,request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors,status=400)
+        
+    def retrieve(self,request,pk=None):
+        project = self.queryset.get(pk=pk)
+        serializer = self.serializer_class(project)
+        return Response(serializer.data)
+    
+    def update(self,request,pk=None):
+        project = self.queryset.get(pk=pk)
+        serializer = self.serializer_class(project, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status = 400)
+        
+    def destroy(self,request,pk=None):
+        project = self.queryset.get(pk=pk)
+        project.delete()
+        return Response(status=204)
