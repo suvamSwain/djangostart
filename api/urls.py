128ed82536c6d8179b22d431ccb23789a3267096
Suvam <suvamgpt8@gmail.com>
2024-04-09 19:54:12 +0530

    deleted main url

--- api/urls.py
+++ api/urls.py
@@ -2,9 +2,9 @@ from django.urls import path,include
 from .views import ProjectViewset,main
 from rest_framework.routers import DefaultRouter
 
-urlpatterns = [
-    path('/main',main)
-]
+# urlpatterns = [
+#     path('/main',main)
+# ]
 
 router = DefaultRouter()
 router.register('project',ProjectViewset, basename='project')
